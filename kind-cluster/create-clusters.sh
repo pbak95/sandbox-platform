e8394672355761450ea9bb75040b2f9fa5fa9273
patryk.bak <patryk.bak@bluesoft.com>
2023-05-25 09:33:32 +0200

    sync changes

--- kind-cluster/create-clusters.sh
+++ kind-cluster/create-clusters.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
 # Create sicily cluster
-kind create cluster --config=kind-config-sicily.yaml --kubeconfig=$(echo $HOME)/.kube/default-config --image kindest/node:v1.23.0 --wait 5m
+kind create cluster --config=kind-config-sicily.yaml --kubeconfig=$(echo $HOME)/.kube/default-config --image kindest/node:v1.27.2 --wait 5m
